12c9ff237069e601b9bf044d2ad9358425e96c1e
SSP6258 <ssp6258@yahoo.com.tw>
2022-04-25 20:51:16 +0800

    0424

--- apps/app_sim_1.py
+++ apps/app_sim_1.py
@@ -210,7 +210,7 @@ def fn_sim_result_render():
 
     x1 = df_all['tick_time']
     y1 = df_all['queue']
-    fig = fn_gen_plotly_scatter(fig, x0, [1 for _ in x0], margin=margin, color='orange', size=13, marker_sym=6, row=2,
+    fig = fn_gen_plotly_scatter(fig, x0, [1 for _ in x0], margin=margin, color='royalblue', size=13, marker_sym=6, row=2,
                                 opacity=0.5, mode='markers', xaxis_range=xaxis_range, name='顧客抵達', legend=True)
 
     x2 = fn_2_timestamp([t+dic_sim_cfg['CASHIER_TIME'] for t in df['done_time'].values])
