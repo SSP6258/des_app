86bac5a1cfb7ca3e35c444551ac6d5869b39e253
SSP6258 <ssp6258@yahoo.com.tw>
2023-02-12 06:14:53 +0800

    0424

--- apps/app_hospital.py
+++ apps/app_hospital.py
@@ -124,7 +124,7 @@ def fn_gen_plotly_gannt(df, x_s, x_e, y, margin=None, color=None, op=None, title
                              showarrow=False,
                              arrowhead=1,
                              arrowsize=2,
-                             xshift=0)
+                             xshift=-30)
 
     fig.update_traces(textposition='outside')
 
