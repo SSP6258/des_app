7f88fe8d39736f82ec1c0aadf6b31c9b05e072fa
SSP6258 <ssp6258@yahoo.com.tw>
2023-02-11 08:18:16 +0800

    0424

--- apps/app_hospital.py
+++ apps/app_hospital.py
@@ -341,9 +341,9 @@ def fn_sim_result_render(df, capacity, x_typ='linear', show_preempt=True):
     st.write(
         '-  🚑  [台灣醫院 急診 檢傷分級: 1.復甦急救 > 2.危急 > 3.緊急 > 4.次緊急 > 5.非緊急](https://www.mgems.org/index.php/zh/question-answer/hospital-ems-triage)')
 
-    st.plotly_chart(fig)
-    st.plotly_chart(fig_q)
-    st.plotly_chart(fig_h)
+    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig_q, use_container_width=True)
+    st.plotly_chart(fig_h, use_container_width=True)
 
     with st.expander('檢視詳細資料'):
         st.write('')
